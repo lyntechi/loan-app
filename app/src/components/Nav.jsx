9a6b33c4ff09c4ca9aec3ff5b10c68fe998be345
Lynda Santiago <lyntechi@gmail.com>
2021-01-05 00:01:47 -0500

    added a logo on nav

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaCarSide } from "react-icons/fa";
+import {AiFillHeart} from 'react-icons/ai'
 
 function Nav() {
   return (
@@ -7,6 +8,7 @@ function Nav() {
       <h1>
         AutoZap Loans <FaCarSide className="car-logo" />
       </h1>
+      <h2>We <AiFillHeart className='heart-logo'/> our customers</h2>
     </div>
   );
 }
