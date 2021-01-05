76bb92d732e59399ad27955b65ed4292b7954e47
Lynda Santiago <lyntechi@gmail.com>
2021-01-04 19:31:26 -0500

    Feature nav (#1)
    * got rid of the unnecessary react starter code
    
    * nav component completed and styled

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Nav />
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaCarSide } from "react-icons/fa";
+
+function Nav() {
+  return (
+    <div className="container">
+      <h1>
+        AutoZap Loans <FaCarSide className="car-logo" />
+      </h1>
+    </div>
+  );
+}
+
+export default Nav;
