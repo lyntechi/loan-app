91160e5743ac0697feb06dc4138c95965eadfc2f
Lynda Santiago <lyntechi@gmail.com>
2021-01-06 14:29:11 -0500

    added extra matches method to separate validation

--- app/src/formSchemas/createAccount.js
+++ app/src/formSchemas/createAccount.js
@@ -4,10 +4,8 @@ export const formSchema = Yup.object().shape({
   username: Yup.string().email().required("Email is required"),
   password: Yup.string()
     .required("Password is required")
-    .matches(
-      /^(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/,
-      "Must contain more than 8 Characters and atleast 1 number"
-    ),
+    .matches(/^[A-Za-z\d@$!%*#?&]{8,}$/, "Must contain more than 8 Characters")
+    .matches(/^.*[0-9].*$/, "Must contain atleast 1 number"),
   confirmation: Yup.string().oneOf(
     [Yup.ref("password"), null],
     "Passwords must match"
